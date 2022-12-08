2ebf0383e2e1d38077682d35938124c531036906
M. Taufiqqurrahman <taufiqqurrahman9872@gmail.com>
2022-12-08 17:04:29 +0700

    filter penelitian pkm dan reimburse

---
+++ app/Views/filter/jenisPenelitian.php
@@ -0,0 +1,11 @@
+<label class="col-md-4 col-lg-2 col-form-label btn btn-primary">Jenis Penelitian</label>
+<div class="col-md-8 col-lg-4">
+    <select class="form-select status-dropdown ">
+        <option value="">Semua</option>
+        <option value="1">Mandiri</option>
+        <option value="2">Semi Mandiri</option>
+        <option value="3">Didanai Institusi</option>
+        <option value="4">Institusi</option>
+        <option value="5">Kerjasama</option>
+    </select>
+</div>
---
+++ app/Views/filter/prosesPKM.php
@@ -0,0 +1,14 @@
+<div label class="col-md-4 col-lg-2 col-form-label btn btn-primary">Status PKM</div>
+
+<div class="col-md-8 col-lg-4">
+    <select class="form-select status-dropdown2 ">
+        <option value="">Semua</option>
+        <option value="Diajukan oleh Dosen">Diajukan oleh Dosen</option>
+        <option value="Pengajuan dalam proses">Pengajuan dalam proses</option>
+        <option value="Pengajuan Disetujui">Pengajuan Disetujui</option>
+        <option value="Kegiatan sedang berlangsung">Kegiatan sedang berlangsung</option>
+        <option value="Kegiatan telah selesai dilaksanakan">Kegiatan telah selesai dilaksanakan</option>
+        <option value="Ditolak oleh Kepala PPPM">Ditolak oleh Kepala PPPM</option>
+        <option value="Alokasi Dana tidak tersedia">Alokasi Dana tidak tersedia</option>
+    </select>
+</div>
