f3c49e40014298b33b737fd81b18a0d1c291fed1
aljaffarsyah <aljaffarsyah10@gmail.com>
2023-01-08 17:16:13 +0700

    update kondef

--- app/Views/dosen/tampilan/kondefPPPM.php
+++ app/Views/dosen/tampilan/kondefPPPM.php
@@ -32,7 +32,116 @@
             <br>
             <p>Berdasarkan peneliti dan pembiayaannya, penyelenggaraan penelitian di lingkungan
                 Politeknik Statistika STIS, dibedakan menjadi:</p>
-            <ol type="1">Penelitian perorangan/kelompok mandiri</ol>
+            <ol type="1">
+                <li>Penelitian perorangan/kelompok mandiri</li>
+                <b>Deskripsi</b>
+                <p>Penelitian yang dalam proses penyelesaian serta publikasinya didanai sendiri oleh
+                    dosen/peneliti.</p>
+                <b>Ketentuan</b>
+                <p>Dalam hal ini, dosen hanya diwajibkan meng-upload ke SIPADU bukti luaran penelitian berupa
+                    artikel (atau link artikel) terpublikasi dalam prosiding seminar nasional, prosiding seminar
+                    internasional, jurnal nasional terakreditasi, atau jurnal internasional. Upload luaran penelitian
+                    sebagai salah satu bukti kinerja tahunan dosen.</p>
+                <li>Penelitian perorangan/kelompok semi mandiri</li>
+                <b>Deskripsi</b>
+                <p>Penelitian yang dalam proses penelitiannya dibiayai secara mandiri oleh dosen atau tim dosen,
+                    namun publikasi dalam bentuk seminar nasional/internasional atau jurnal nasional/
+                    internasional didanai oleh Politeknik Statistika STIS</p>
+                <li>Penelitian perorangan/kelompok didanai institusi</li>
+                <b>Deskripsi</b>
+                <p>Penelitian yang dalam proses penyelesaian maupun publikasinya didanai oleh Politeknik
+                    Statistika STIS. Penelitian yang dimaksud termasuk dalam jenis penelitian dasar dan penelitian
+                    terapan yang pelaksanaan penelitiannya dalam satu tahun anggaran.</p>
+                <li>Penelitian perorangan/kelompok institusi</li>
+                <p>Penelitian Institusi adalah penelitian yang dilakukan oleh institusi di dalam lingkungan Politeknik Statistika STIS, diantaranya penelitian yang dilaksanakan oleh Unit Kajian atau unit lain yang ditunjuk. Tujuan penelitian institusi adalah untuk memberikan masukan dan manfaat bagi pengembangan institusi, instansi induk serta meningkatkan sumber daya manusia yang ada didalamnya. Dalam penelitian ini termasuk juga jenis penelitian pengembangan, yang waktu pelaksanaannya dimungkinkan beberapa tahun (multiyears).</p>
+
+                <li>Penelitian kerjasama</li>
+                <b>Deskripsi</b>
+                <p>Penelitian yang dalam proses penyelesaian serta publikasinya didanai melalui mekanisme
+                    kerjasama.
+                </p>
+                <b>Ketentuan</b>
+                <p>Dalam hal adanya keikutsertaan pendanaan dari Politeknik Statistika STIS, dalam publikasi
+                    penelitian diharuskan ikut mencantumkan Politeknik Statistika STIS dalam acknowledgement
+                    atau sumber pendanaan (sources of funding). Dalam hal dihasilkannya hak cipta atau paten
+                    dari penelitian yang ikut didanai Politeknik Statistika STIS tersebut agar memberikan juga
+                    porsi hak kepemilikan dan/atau akses kepada Politeknik Statistika STIS.</p>
+            </ol>
+
+            <h1>Pengabdian Kepada Masyarakat (PKM)</h1>
+            <p>Pengabdian Kepada Masyarakat (PKM) merupakan salah satu bentuk Tri Dharma perguruan
+                tinggi selain pengajaran dan penelitian. PKM dilaksanakan sebagai wadah bagi dosen untuk
+                memberikan kontribusi terhadap masyarakat sesuai bidang keilmuan dan pengalaman yang
+                dimiliki. </p>
+            <p>Kegiatan PKM di lingkungan Politeknik Statistika STIS (Polstat STIS) diarahkan pada
+                kegiatan yang mendukung kegiatan BPS dan statistika nasional, yakni sebagai berikut: </p>
+            <ul>
+                <li>penggunaan data statistik</li>
+                <li>pembinaan statistik sektoral</li>
+                <li>pengembangan wilayah</li>
+                <li>pemberdayaan masyarakat</li>
+            </ul>
+            <p>Adapun bentuk kegiatan PKM dosen di lingkungan Polstat STIS tersebut terdiri dari:</p>
+            <ol type="1">
+                <li>terstruktur</li>
+                <b>deskripsi</b>
+                <p>Kegiatan PKM yang kegiatannya dirancang oleh pengelola atau institusi. Kegiatan
+                    dapat berkolaborasi dengan mahasiswa, instansi induk, atau instansi lain. </p>
+                <b>Bentuk kegiatan</b>
+                <p>Beberapa contoh kegiatan di sini antara lain;</p>
+                <ul>
+                    <li>Pemberdayaan Desa Tertinggal (PDT) yang merupakan kolaborasi dengan kegiatan
+                        mahasiswa </li>
+                    <li>Penyelenggaraan Webinar Statistika terstruktur sebagai bentuk pengabdian kepada
+                        masyarakat </li>
+                    <li>Keikutsertaan dalam Program Desa Cinta Statistik (DESA CANTIK) dari BPS</li>
+                    <li>Pemasyarakatan Statistik lainnya yang dikelola oleh institusi
+                    </li>
+                </ul>
+                <b>Pendanaan </b>
+                <p>Pembiayaan ditanggung oleh institusi sesuai dengan ketentuan yang berlaku.
+                    Komponen yang dibiayai mencakup transportasi, penginapan, uang harian, dan
+                    komponen lain yang dapat dipertanggungjawabkan</p>
+
+                <li>Perorangan/Kelompok</li>
+                <b>deskripsi </b>
+                <p>Kegiatan PKM yang kegiatannya merupakan inisiatif dari dosen dengan fasilitas
+                    dan/atau pembiayaan dari institusi. </p>
+                <b>Bentuk kegiatan</b>
+                <p>Bentuk kegiatan menyesuaikan dengan kondisi dan kreativitas dosen dalam bentuk
+                    literasi statistik berupa pendampingan, pelatihan, penyelesaian official statistics,
+                    sampling, data science, komputasi statistik, atau bentuk lainnya.
+                    Dapat dilaksanakan dengan:
+                </p>
+                <ul>
+                    <li>Di luar kota dan menginap</li>
+                    <li>Di dalam Kota Jakarta, Depok, Bekasi, Bogor, Tangerang yang tidak menginap </li>
+                    <li>Secara daring </li>
+                </ul>
+                <b>Persyaratan</b>
+                <ol type="a">
+                    <li>Fungsional dosen atau calon fungsional dosen</li>
+                    <li>Dapat perorangan atau tim</li>
+                    <li>Pengusul adalah fungsional dosen sebagai ketua tim, dengan dosen dan/atau calon
+                        fungsional dosen sebagai anggota </li>
+                    <li>Paling banyak satu kali satu tahun</li>
+                </ol>
+                <b>Pendanaan</b>
+                <p>Komponen kegiatan PKM yang dibiayai meliputi: </p>
+                <ol type="a">
+                    <li>Komponen biaya perjalanan untuk 3 hari (luar kota) dan 1 hari (dalam kota)</li>
+                    <li>Biaya lainnya yang dapat dipertanggungjawabkan</li>
+                    <li>Politeknik Statistika STIS tidak memberikan honor sebagai narasumber bagi
+                        dosen yang melaksanakan PKM</li>
+                </ol>
+
+                <li>Mandiri
+                </li>
+                <b>deskripsi </b>
+                <p>kegiatan PKM yang merupakan inisiatif dosen dan tidak dibiayai oleh institusi (tanpa
+                    menggunakan fasilitas dana dari institusi)</p>
+
+            </ol>
         </div>
 
     </section>
